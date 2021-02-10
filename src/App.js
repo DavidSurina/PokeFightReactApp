7fdb14dde0d2c38e26ff9501c606117df76a0f6f
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-10 08:56:53 +0100

    add title to / route

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <Switch>
         <Route path="/pokemon/:id/:info"></Route>
         <Route path="/pokemon/:id"></Route>
         <Route path="/pokemon"></Route>
-        <Route path="/"></Route>
+        <Route path="/">
+          <h1>PokeFight</h1>
+        </Route>
       </Switch>
     </div>
   );
