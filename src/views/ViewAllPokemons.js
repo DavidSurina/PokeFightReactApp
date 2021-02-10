8528e94fab476fb4b56693014c8f5cc20448a37d
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-10 12:29:18 +0100

    map first ten pokemons

--- src/views/ViewAllPokemons.js
+++ src/views/ViewAllPokemons.js
@@ -20,36 +20,18 @@ const ViewAllPokemons = () => {
     <>
       <h1>All Pokemons</h1>
       <div className="pokemon-list">
+      {/* FIXME: get better solution for limiting/offset/streaming */}
         <ul>
-        {/* for performance reasons only the first 3 Pokemons are listed */}
-          {pokemonList 
-            ? <>
-              <li key={pokemonList[0].id}>
-                  <a href={`/pokemons/${pokemonList[0].id}`}>
-                    {pokemonList[0].name.english}
-                  </a>
-                </li> 
-                <li key={pokemonList[1].id}>
-                  <a href={`/pokemons/${pokemonList[1].id}`}>
-                    {pokemonList[1].name.english}
-                  </a>
-                </li>
-                <li key={pokemonList[2].id}>
-                  <a href={`/pokemons/${pokemonList[2].id}`}>
-                    {pokemonList[2].name.english}
-                  </a>
-                </li>
-              </>
-            : null }
-
-        {/* FIXME: we need data streaming. Browser is  */}
-{/*           {pokemonList.map((pokemon) => {
+          {pokemonList
+            ? pokemonList.slice(0, 10).map((pokemon) => {
             return <li key={pokemon.id}>
               <a href={`/pokemons/${pokemon.id}`}>
                 {pokemon.name.english}
               </a>
             </li>
-          })} */}
+          })
+          : null
+          }
         </ul>
       </div>
     </>
