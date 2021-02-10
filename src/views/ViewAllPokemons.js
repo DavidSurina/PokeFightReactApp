5621f991e94133f8831ba70687a4381cc4a031c7
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-10 11:44:38 +0100

    implement simple view of all pokemons

---
+++ src/views/ViewAllPokemons.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+// Import api functionality
+import Api from "../api/index";
+
+const ViewAllPokemons = () => {
+  const [pokemonList, setPokemonList] = useState([{id: 0, name: {english: "default"}}]);
+
+  useEffect(() => {
+    Api.getAllPokemons()
+      .then((res)=>{
+        setPokemonList(res);
+      })
+      .catch((err)=>{
+        console.error(err)
+      })
+  },[]);
+
+  console.log("pokemonList", pokemonList);
+
+  return(
+    <>
+      <h1>All Pokemons</h1>
+      <div className="pokemon-list">
+        <ul>
+        {/* for performance reasons only the first 3 Pokemons are listed */}
+          <li key={pokemonList[0].id}>
+            <a href={`/pokemons/${pokemonList[0].id}`}>
+              {pokemonList[0].name.english}
+            </a>
+          </li>
+          <li key={pokemonList[1].id}>
+            <a href={`/pokemons/${pokemonList[1].id}`}>
+              {pokemonList[1].name.english}
+            </a>
+          </li>
+          <li key={pokemonList[2].id}>
+            <a href={`/pokemons/${pokemonList[2].id}`}>
+              {pokemonList[2].name.english}
+            </a>
+          </li>
+        {/* FIXME: we need data streaming. Browser is  */}
+{/*           {pokemonList.map((pokemon) => {
+            return <li key={pokemon.id}>
+              <a href={`/pokemons/${pokemon.id}`}>
+                {pokemon.name.english}
+              </a>
+            </li>
+          })} */}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default ViewAllPokemons;
