93ed6c5241489cbba177bb13558e508ffa201e46
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-10 10:36:25 +0100

    implement api fetch for getAllPokemons route

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,22 @@
 import axios from "axios";
+
+/* TODO: needs to be replaced w/ the heroku deployed app URL */
+const endpoint = "http://localhost:4000/";
+const Api = {
+  getAllPokemons: async () => {
+    try {
+      const response = await axios.get(`${endpoint}pokemons`);
+      if (response.data.data) {
+        return response.data.data;
+      } else {
+        console.log("no poke data");
+        return [];
+      }
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  },
+};
+
+export default Api;
