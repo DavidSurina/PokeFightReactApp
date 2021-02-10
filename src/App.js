ef9a07e2eb8a5da43f09e760bc6e36aed363a626
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-10 10:14:10 +0100

    changed naming of pathes to plural and import View for all Pokemons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Switch, Route } from 'react-router-dom';
+/* views */
+import ViewAllPokemons from "./views/ViewAllPokemons"
+
 /* styles */
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
       <Switch>
-        <Route path="/pokemon/:id/:info"></Route>
-        <Route path="/pokemon/:id"></Route>
-        <Route path="/pokemon"></Route>
+        <Route path="/pokemons/:id/:info"></Route>
+        <Route path="/pokemons/:id"></Route>
+        <Route path="/pokemons">
+          <ViewAllPokemons />
+        </Route>
         <Route path="/">
           <h1>PokeFight</h1>
         </Route>
