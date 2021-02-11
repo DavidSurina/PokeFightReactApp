66c524a95ad3a57a7c08f3538b78bd41e451ab20
DavidSurina <david.surina22@gmail.com>
2021-02-11 18:11:12 +0100

    created folder for pictures, added 2 components and css for them, and incorporated them into app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import { Switch, Route } from 'react-router-dom';
+/* components */
+import InputForm from "./components/InputForm";
+import Footer from "./components/Footer";
 /* views */
 import ViewAllPokemons from "./views/ViewAllPokemons"
 
+/* Logo */
+import logo from "./img/pokefight_logo.png";
 /* styles */
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/pokemons/:id/:info"></Route>
-        <Route path="/pokemons/:id">
-          {/*Pokemon info by id*/}
-        </Route>
-        <Route path="/pokemons/fight">
-          {/*Pokemon Fight*/}
-        </Route>
-        <Route path={["/","/pokemons"]}>
-          {/*all pokemon*/}
-          <ViewAllPokemons />
-        </Route>
-      </Switch>
+    <div classNameName="App">
+      <header className="header">
+          <div className="logo-wrapper">
+              <img className="pokefight-logo" src={logo} />
+          </div>
+          <InputForm />
+      </header>
+      <main class="main">
+        <Switch>
+          <Route path="/pokemons/:id/:info"></Route>
+          <Route path="/pokemons/:id">
+            {/*Pokemon info by id*/}
+          </Route>
+          <Route path="/pokemons/fight">
+            {/*Pokemon Fight*/}
+          </Route>
+          <Route path={["/","/pokemons"]}>
+            {/*all pokemon*/}
+            <ViewAllPokemons />
+          </Route>
+        </Switch>
+      </main>
+      <Footer />
     </div>
   );
 }
