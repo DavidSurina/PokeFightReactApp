d9db2f0ad0b376f7d1515d583e6f68e2c539268e
DavidSurina <david.surina22@gmail.com>
2021-02-14 16:25:12 +0100

    added if/else in pokefight to prevent error

--- src/components/PokemonFight/index.js
+++ src/components/PokemonFight/index.js
@@ -2,20 +2,24 @@ import "./style.css";
 
 export default function PokemonFight({fightingPoke}) {
     console.log(fightingPoke)
-    return (
-        <div className="pokefight-wrapper">
-            <div className="pokefight-top-wrapper">
-                <div className="pokefight-top-name-wrapper">
-                    <h2>{fightingPoke[1].name.english}</h2>
+    if(fightingPoke.length === 2) {
+        return (
+            <div className="pokefight-wrapper">
+                <div className="pokefight-top-wrapper">
+                    <div className="pokefight-top-name-wrapper">
+                        <h2>{fightingPoke[1].name.english}</h2>
+                    </div>
+                    <img className="pokefight-top-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${fightingPoke[1].id}.png`}/>
                 </div>
-                <img className="pokefight-top-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${fightingPoke[1].id}.png`}/>
-            </div>
-            <div className="pokefight-bot-wrapper">
-                <img className="pokefight-bot-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${fightingPoke[0].id}.png`}/>
-                <div className="pokefight-bot-name-wrapper">
-                    <h2>{fightingPoke[0].name.english}</h2>
+                <div className="pokefight-bot-wrapper">
+                    <img className="pokefight-bot-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${fightingPoke[0].id}.png`}/>
+                    <div className="pokefight-bot-name-wrapper">
+                        <h2>{fightingPoke[0].name.english}</h2>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (<h1>Sorry select pokemon to fight</h1>)
+    } 
 }
