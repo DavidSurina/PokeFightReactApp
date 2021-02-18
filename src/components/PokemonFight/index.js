d939fb4c5da40842954a2f5bcf6556b7d9bfe27b
David Surina <david.surina22@gmail.com>
2021-02-18 15:19:38 +0100

    added missigno pic, added small animation to the fight

--- src/components/PokemonFight/index.js
+++ src/components/PokemonFight/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 
 export default function PokemonFight({fightingPoke}) {
+    
+
     console.log(fightingPoke)
     if(fightingPoke.length === 2) {
         return (
@@ -8,6 +10,12 @@ export default function PokemonFight({fightingPoke}) {
                 <div className="pokefight-top-wrapper">
                     <div className="pokefight-top-name-wrapper">
                         <h2>{fightingPoke[1].name.english}</h2>
+                        <p class="pokefight-stats">Hp: {fightingPoke[1].base.HP}</p>
+                        <p class="pokefight-stats">Speed: {fightingPoke[1].base.Speed}</p>
+                        <p class="pokefight-stats">Attack: {fightingPoke[1].base.Attack}</p>
+                        <p class="pokefight-stats">Defense: {fightingPoke[1].base.Defense}</p>
+                        <p class="pokefight-stats">Sp.Attack: {fightingPoke[1].base["Sp. Attack"]}</p>
+                        <p class="pokefight-stats">Sp.Defense: {fightingPoke[1].base["Sp. Defense"]}</p>
                     </div>
                     <img className="pokefight-top-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${fightingPoke[1].id}.png`}/>
                 </div>
@@ -15,6 +23,12 @@ export default function PokemonFight({fightingPoke}) {
                     <img className="pokefight-bot-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${fightingPoke[0].id}.png`}/>
                     <div className="pokefight-bot-name-wrapper">
                         <h2>{fightingPoke[0].name.english}</h2>
+                        <p class="pokefight-stats">Hp: {fightingPoke[0].base.HP}</p>
+                        <p class="pokefight-stats">Speed: {fightingPoke[0].base.Speed}</p>
+                        <p class="pokefight-stats">Attack: {fightingPoke[0].base.Attack}</p>
+                        <p class="pokefight-stats">Defense: {fightingPoke[0].base.Defense}</p>
+                        <p class="pokefight-stats">Sp.Attack: {fightingPoke[0].base["Sp. Attack"]}</p>
+                        <p class="pokefight-stats">Sp.Defense: {fightingPoke[0].base["Sp. Defense"]}</p>
                     </div>
                 </div>
             </div>
