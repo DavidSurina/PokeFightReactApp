0dbd379abbe498da8c65ea52be0bfbe7f84ce398
Stefanibus <stefan.brueck@posteo.de>
2021-02-18 13:26:26 +0100

    Modal now supports the user while choosing Pokemons, still unfinished work

--- src/views/ViewAllPokemons.js
+++ src/views/ViewAllPokemons.js
@@ -7,18 +7,17 @@ import CenteredGrid from '../components/PokeGrid';
 const ViewAllPokemons = ({pokemonList, fightPokemon, setFightPokemon}) => {
 
   const [myPokemon, setMyPokemon]   = useState()
-  const [pokeInfo, setPokeInfo] = useState('');
+  const [open, setOpen]             = useState(false);
+  const [pokeInfo, setPokeInfo]     = useState({textInfo: ' ', chooseOrFight: true,  firstOrSecondChoice: false,  buttonShow: true });
 
-  const [open, setOpen] =  useState(false);
   const handleOpenParent  = () => { setOpen(true); };
   const handleCloseParent = () => { setOpen(false); };
 
-
   useEffect(() => {
-     setPokeInfo( `Choose two Pokemons and... \nlet them fight!` );
+     setPokeInfo( {...pokeInfo, textInfo: `Choose two Pokemons and... \nlet them fight!`, buttonShow: true });
   }, []);
 
-
+// console.log('\npokeInfo.buttonShow', pokeInfo.buttonShow, '\npokeInfo.chooseOrFight ', pokeInfo.chooseOrFight, '\npokeInfo.firstOrSecond ', pokeInfo.firstOrSecond, '\npokeInfo.textInfo', pokeInfo.textInfo )
 
   const fightSelectionController = (arr, pokemon) => {
 
@@ -26,17 +25,19 @@ const ViewAllPokemons = ({pokemonList, fightPokemon, setFightPokemon}) => {
     if(arr.length < 1) {
       nArray.push(pokemon);
       setFightPokemon(nArray)
-      setPokeInfo( `Okay, cool! \nYour first Pokemon is: ${nArray[0].name.english}. \nNow Choose your second Pokemon! ` )
+      setPokeInfo({ ...pokeInfo, textInfo: `Okay, cool! \nYour first Pokemon is: ${nArray[0].name.english}. \nNow Choose your second Pokemon! `, buttonShow: false, firstOrSecondChoice: true,  } )
+
     } else if(arr.length === 1) {
       nArray[1] = pokemon;
       setFightPokemon(nArray);
-      setPokeInfo( `${nArray[0].name.english} is ready to fight against ${nArray[1].name.english}! \nHit the Fight Button! ` );
+
+      setPokeInfo({...pokeInfo, textInfo: `Okay, ${nArray[0].name.english} will fight against ${nArray[1].name.english}. \nHit the Fight Button! `, buttonShow: false, chooseOrFight: false} );
     } else  {
       handleCloseParent()
       // revert values back
-      setPokeInfo('Choose two Pokemons! \nYou can let them fight against one another!')
-      // this should only kick in if the user really hits the fight button
-      setFightPokemon([])
+       setPokeInfo({...pokeInfo, textInfo: 'Choose two Pokemons! \nYou can let them fight against one another!', buttonShow: true, firstOrSecondChoice: false, } )
+       // reset (setFightPokemon to empty Array )should only happen if the user really hits the fight button
+       setFightPokemon([])
     }
   }
   return(
@@ -51,6 +52,7 @@ const ViewAllPokemons = ({pokemonList, fightPokemon, setFightPokemon}) => {
             fightPokemon={fightPokemon}
             fightSelectionController={fightSelectionController}
             pokeInfo={pokeInfo}
+             setPokeInfo={setPokeInfo}
             />
       <div className="pokemon-list">
         <CenteredGrid
